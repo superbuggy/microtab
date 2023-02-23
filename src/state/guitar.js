0f8f424563cdb77bab23f4bf8e3d8ca2c60d11fc
superbuggy <jpqreichard@gmail.com>
2023-02-23 12:59:04 -0500

    ✨ Provide tuning in guitar state

--- src/state/guitar.js
+++ src/state/guitar.js
@@ -1,11 +1,24 @@
 import { ref } from "vue";
 
+
 const DEFAULT_STRING_QUANTITY = 6;
 const DEFAULT_OCTAVE_DIVISONS = 24;
+const TUNING = ["B1", "E2", "A2", "D3", "F#3", "B3"];
 
 export function useGuitar() {
   const stringQuantity = ref(DEFAULT_STRING_QUANTITY);
   const divisonsPerOctave = ref(DEFAULT_OCTAVE_DIVISONS);
+  const stringNumbers = Array.from({ length: stringQuantity.value }).map(
+    (_, index, { length }) => length - index
+  );
+  const tuning = ref(
+    Object.fromEntries(
+      stringNumbers.map((stringNumber, index) => [
+        `string${stringNumber}`,
+        TUNING[index],
+      ])
+    )
+  );
 
   // function updateStringQuantity(quantity) {
   //   stringQuantity.value = quantity;
@@ -17,6 +30,8 @@ export function useGuitar() {
   return {
     stringQuantity,
     divisonsPerOctave,
+    tuning,
+    stringNumbers,
     // updateStringQuantity,
     // updatedivisonsPerOctave,
   };
