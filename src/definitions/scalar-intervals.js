8115f2de6f9348054cebb52b3122a7eab230d403
superbuggy <jpqreichard@gmail.com>
2023-03-09 21:24:26 -0500

    🌱 Add scale definitions

--- src/definitions/scalar-intervals.js
+++ src/definitions/scalar-intervals.js
@@ -125,3 +125,36 @@ export const scalarIntervallicDistances = {
   Aeolian: [4, 2, 4, 4, 2, 4, 4],
   Locrian: [2, 4, 4, 2, 4, 4, 4],
 };
+
+const intervallicDistances = (pitchNumbers) =>
+  pitchNumbers.slice(1).reduce((intervals, pitchNumber, index) => {
+    intervals.push(pitchNumber - pitchNumbers[index]);
+    return intervals;
+  }, []);
+
+// const compareCentsForEDO = (centsValues, edo) =>
+//   centsValues.map((i) => Math.round(i / (1200 / edo)) * (1200 / edo) - i);
+
+const centsToEDO = (centsValues, edo) =>
+  centsValues.map((i) => Math.round(i / (1200 / edo)));
+
+const approxBohlenPierceCents = [
+  0, 133.2, 301.8, 435.1, 582.5, 736.9, 884.4, 1017.0, 1165.0, 1319.4, 1466.9,
+  1600.1, 1768.7, 1900,
+];
+const modifiedBohlenPierceCents = [
+  0, 133.2, 301.8, 435.1, 590.2, 745.8, 884.4, 1035.0 /* neutral 7th */, 1165.0,
+  1328.3, 1453.8, 1600.1, 1768.7, 1900,
+];
+
+const overTonePrimes = [0, 386, 702, 969, 551, 840, 105, 297, 1200].sort(
+  (a, b) => a - b
+); // octave reduced
+
+scalarIntervallicDistances["Bohlen-Pierce 24 EDO Approximation"] =
+  intervallicDistances(centsToEDO(approxBohlenPierceCents, 24));
+scalarIntervallicDistances["Bohlen-Pierce 24 EDO (Neutral 7th)"] =
+  intervallicDistances(centsToEDO(modifiedBohlenPierceCents, 24));
+scalarIntervallicDistances["Prime Overtone 24 EDO"] = intervallicDistances(
+  centsToEDO(overTonePrimes, 24)
+);
