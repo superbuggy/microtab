fc3180e8fb876ab9655b583a7a932c8c98200508
superbuggy <jpqreichard@gmail.com>
2023-03-09 21:26:40 -0500

    ✨ Encapsulate temperament functions in extended class

--- src/state/temperament.js
+++ src/state/temperament.js
@@ -35,20 +35,33 @@ const tet24schema = {
     "B‡": ["B", 50],
   },
 };
-export function useTemperament() {
-  const tet24 = new Temperament(tet24schema);
-  const pitchFromNote = (pitch) => {
-    const [, pitchClass, octave] = pitch.match(/([^0-9]+)(\d)/);
-    return tet24.getPitch(pitchClass, octave);
-  };
-  const pitchesFromNotes = (pitches) => pitches.map(pitchFromNote);
-  const noteNames = tet24
-    .getOctaveRange(4)
-    .flatMap((octaveNumber) =>
-      tet24.noteNames.map((noteName) => noteName + octaveNumber)
-    );
-  const noteFromStepsAbove = (referenceNoteName, stepsAbove) => {
-    const referenceNoteNameIndex = noteNames.findIndex(
+
+class TET extends Temperament {
+  constructor(temperamentData) {
+    super(temperamentData);
+  }
+
+  get pitchClassNames() {
+    return super.noteNames;
+  }
+
+  get pitchNames() {
+    return Array.from({ length: 8 })
+      .map((_, octaveNumber) => octaveNumber)
+      .flatMap((octaveNumber) =>
+        this.pitchClassNames.map(
+          (pitchClassName) => pitchClassName + octaveNumber
+        )
+      );
+  }
+
+  frequencyFor(pitchName) {
+    const [, pitchClass, octave] = pitchName.match(/([^0-9]+)(\d)/);
+    return super.getPitch(pitchClass, octave);
+  }
+
+  noteFromStepsAbove(referenceNoteName, stepsAbove) {
+    const referenceNoteNameIndex = this.pitchNames.findIndex(
       (matchedNoteName) => matchedNoteName === referenceNoteName
     );
     if (referenceNoteName === -1)
@@ -56,24 +69,77 @@ export function useTemperament() {
         "Note not found" + stepsAbove + " steps above " + referenceNoteName
       );
 
-    const foundNote = noteNames[referenceNoteNameIndex + stepsAbove];
+    const foundNote = this.pitchNames[referenceNoteNameIndex + stepsAbove];
     if (!foundNote)
       throw new Error(
         "Note out of bounds" + stepsAbove + " steps above " + referenceNoteName
       );
 
     return foundNote;
-  };
+  }
+  distanceBetweenNotes(lowerNote, higherNote) {
+    return (
+      this.pitchNames.indexOf(higherNote) - this.pitchNames.indexOf(lowerNote)
+    );
+  }
+}
 
+function noteInTET(temperament) {
+  return class Note {
+    constructor(pitchName) {
+      const [, pitchClass, octave] = pitchName.match(/([^0-9]+)(\d)/);
+
+      if (temperament.noteNames.indexOf(pitchClass) === -1) {
+        throw new Error(
+          `🚫🎵 Note ${pitchClass} not contained within Temperament ${temperament.name}.`
+        );
+      }
+      this.octave = octave;
+      this.pitchClass = pitchClass;
+      this.pitch = pitchName;
+    }
+
+    get frequency() {
+      return temperament.frequencyFor(this.pitch);
+    }
+
+    get temperament() {
+      return temperament;
+    }
+
+    get absolutePitchNumber() {
+      return temperament.pitchNames.findIndex(
+        (pitchName) => pitchName === this.pitch
+      );
+    }
+
+    get pitchClassNumber() {
+      return temperament.noteNames.indexOf(this.pitchClass);
+    }
+  };
+}
+
+export function useTemperament() {
+  const tet24 = new TET(tet24schema);
+  const Note = noteInTET(tet24);
+
+  const noteFromStepsAbove = (referenceNoteName, stepsAbove) =>
+    tet24.noteFromStepsAbove(referenceNoteName, stepsAbove);
   const distanceBetweenNotes = (lowerNote, higherNote) =>
-    noteNames.indexOf(higherNote) - noteNames.indexOf(lowerNote);
+    tet24.distanceBetweenNotes(lowerNote, higherNote);
+  const noteNames = tet24.pitchNames;
+  const pitchClassNames = tet24.pitchClassNames;
+  const notes = tet24.pitchNames.map((pitchName) => new Note(pitchName));
+  window.tet24 = tet24;
+  window.Note = Note;
 
   return {
     tet24,
     noteNames,
-    pitchFromNote,
-    pitchesFromNotes,
+    notes,
+    Note,
     noteFromStepsAbove,
     distanceBetweenNotes,
+    pitchClassNames
   };
 }
