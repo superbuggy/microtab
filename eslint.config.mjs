d5dcd0f2d4d5fdec1b71c77a5e5b6539b7488a3f
superbuggy <jpqreichard@gmail.com>
2024-11-04 18:22:04 -0500

    🔧 Configure ESLint vue plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,34 @@
-
-
-
-// @ts-check
-
+ //@ts-check
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
 import eslintConfigPrettier from 'eslint-config-prettier';
+import parserVue from 'vue-eslint-parser';
 
-export default tseslint.config({
-  ...eslint.configs.recommended,
+export default tseslint.config(
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  ...eslintConfigPrettier,
-  ...pluginVue.configs['flat/essential']
-});
+  eslintConfigPrettier,
+  // @ts-expect-error - Missing types
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['*.vue', '**/*.vue'],
+    languageOptions: {
+      parser: parserVue,
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+    name: 'vue',
+    plugins: {
+      vue: pluginVue,
+    },
+  },
+  {
+    rules: {
+      rules: {
+        "@typescript-eslint/no-explicit-any": null
+      }
+    }
+  }
+);
